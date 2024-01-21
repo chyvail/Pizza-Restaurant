f32ef5bee27eb7020552b88c47ef8ff4f002a3f1
joshua chivile <joshuachivile@gmail.com>
2024-01-21 22:47:19 +0300

    Created Restaurant,Pizza and Restaurant Pizza Models

---
+++ backend/models.py
@@ -0,0 +1,39 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy_serializer import SerializerMixin
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+class RestaurantPizza(db.Model):
+    __tablename__ = 'restaurant_pizzas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    @validates('price')
+    def validate_price(self, key, price):
+        if len(price) < 30:
+            raise ValueError("Description must have more than 30 characters")
+        return price
+
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__ = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    address = db.Column(db.String(255))
+    pizzas = db.relationship('Pizza', secondary='restaurant_pizzas', backref='restaurants')
+
+class Pizza(db.Model, SerializerMixin):
+    __tablename__ = "pizzas"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    ingredients = db.Column(db.String(255))
