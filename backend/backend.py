1d3abf14aaf01229717ecb96e2daa680787a2332
joshua chivile <joshuachivile@gmail.com>
2024-01-21 23:35:12 +0300

    Home Route working

--- backend/backend.py
+++ backend/backend.py
@@ -2,6 +2,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from http import HTTPStatus
 
 from models import db
 
@@ -15,6 +16,23 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+
+class Home(Resource):
+    
+    def get(self):
+         
+        response_dict = {
+            "Message": "Pizza Resturant API",
+        }
+        
+        response = make_response(
+            response_dict,
+            HTTPStatus.OK,
+        )
+
+        return response
+
+api.add_resource(Home, '/')
 
 if __name__ == '__main__':
     app.run(port=5555,debug=True)
