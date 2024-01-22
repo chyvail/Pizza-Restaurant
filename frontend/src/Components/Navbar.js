f974fd97c7174eef0bc066f0e6c5f48c1c72bee2
joshua chivile <joshuachivile@gmail.com>
2024-01-22 05:20:41 +0300

    Added Navbar Component

---
+++ frontend/src/Components/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar(){
+    return (
+        <>
+        <div className="text-center nav-bar">
+            <h4>PIZZA RESTAURANT MINI-APP 🍕</h4>
+        </div>
+        </>
+    )
+}
